43ea0e11c08f08408a1fa89eb3bf5b5f1d6723f2
Harshil Agrawal (harshil1712) <github@harshil.dev>
2023-10-09 12:43:28 +0200

    🔨 Fix build issue

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,13 @@
 import { defineConfig } from 'astro/config';
+import nodejs from '@astrojs/node';
 
 import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
   output: 'server',
+  adapter: nodejs({
+    mode: 'standalone'
+  }),
   integrations: [tailwind()]
 });
